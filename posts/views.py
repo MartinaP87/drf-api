b72f0cb46c28b38fa7e223c7232b750fb706f3f2
MartinaP87 <bleach1987@hotmail.it>
2023-01-11 08:52:05 +0000

    created the  PostList view

--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,26 @@
-from django.http import Http404
-from rest_framework import status
+# from django.http import Http404
+from rest_framework import status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import PostSerializer
 from .models import Post
-from drf_api.permissions import IsOwnerOrReadOnly
+# from drf_api.permissions import IsOwnerOrReadOnly
 
 class PostList(APIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get(self, request):
         posts = Post.objects.all()
         serializer = PostSerializer(
             posts, many=True, context={'request': request})
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PostSerializer(
+            data=request.data, context={'request': request}
+            )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
