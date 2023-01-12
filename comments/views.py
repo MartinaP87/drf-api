86d9338c823f7f0fdd4048cdb1da6b0dd57c837b
MartinaP87 <bleach1987@hotmail.it>
2023-01-12 15:34:49 +0000

    Create LikeList and LikeDetail views

--- comments/views.py
+++ comments/views.py
@@ -13,6 +13,6 @@ class CommentList(generics.ListCreateAPIView):
 
 
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsOwnerOrReadOnly()]
+    permission_classes = [IsOwnerOrReadOnly]
     serializer_class = CommentDetailSerializer
     queryset = Comment.objects.all()
