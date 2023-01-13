a2a58f849dc11026a8d8ff3022792a94cc12ec5e
MartinaP87 <bleach1987@hotmail.it>
2023-01-13 13:16:51 +0000

    Add new fields to Profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-from rest_framework import status, generics, permissions
+from rest_framework import generics, filters
+from django.db.models import Count
 from .serializers import ProfileSerializer
 from .models import Profile
 from drf_api.permissions import IsOwnerOrReadOnly
@@ -6,10 +7,30 @@ from drf_api.permissions import IsOwnerOrReadOnly
 
 class ProfileList(generics.ListCreateAPIView):
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('created_at')
+
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner_following_created_at',
+        'owner_followed_created_at'
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
-    permission_classes= [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
