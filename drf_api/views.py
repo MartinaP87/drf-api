bbe29ba4f9ad20bc0238ace892e1153d2cc0ad07
MartinaP87 <bleach1987@hotmail.it>
2023-01-26 15:32:03 +0000

    minor changes

--- drf_api/views.py
+++ drf_api/views.py
@@ -1,8 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .settings import (
-    JWT_AUTH_SECURE, JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE)
-
+    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
+    JWT_AUTH_SECURE,
+)
 
 
 @api_view()
@@ -11,6 +12,8 @@ def root_route(request):
         "message": "Welcome to my drf API!"
     })
 
+
+# dj-rest-auth logout view fix
 @api_view(['POST'])
 def logout_route(request):
     response = Response()
@@ -21,7 +24,7 @@ def logout_route(request):
         expires='Thu, 01 Jan 1970 00:00:00 GMT',
         max_age=0,
         samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE
+        secure=JWT_AUTH_SECURE,
     )
     response.set_cookie(
         key=JWT_AUTH_REFRESH_COOKIE,
@@ -30,6 +33,6 @@ def logout_route(request):
         expires='Thu, 01 Jan 1970 00:00:00 GMT',
         max_age=0,
         samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE
+        secure=JWT_AUTH_SECURE,
     )
     return response
